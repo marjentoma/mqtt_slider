3980980f9324efdf05fbc28c454ad6756fc1a4fc
Marjorie Entoma <19104863@usc.edu.ph>
2021-05-28 13:44:31 +0800

    First change

--- entoma-mqtt-sliders-clue.py
+++ entoma-mqtt-sliders-clue.py
@@ -1,7 +1,9 @@
 """
 To get started, check out the "Device Simulator Express: Getting Started" command in the command pallete, which you can access with `CMD + SHIFT + P` For Mac and `CTRL + SHIFT + P` for Windows and Linux.
+
 To learn more about the CLUE and CircuitPython, check this link out:
 https://learn.adafruit.com/adafruit-clue/circuitpython
+
 Find example code for CPX on:
 https://blog.adafruit.com/2020/02/12/three-fun-sensor-packed-projects-to-try-on-your-clue-adafruitlearningsystem-adafruit-circuitpython-adafruit/
 """
@@ -10,53 +12,55 @@ from adafruit_clue import clue
 import paho.mqtt.client as mqtt
 
 def display_text(temp):
-    clue_data[0].text = "Accel: {} {} {} m/s^2".format(temp["slider/xaccelerometer"] , temp["slider/yaccelerometer"], temp["slider/zaccelerometer"])
-    clue_data[1].text = "Gyro: {} {} {} dps".format(temp["slider/xgyroscope"], temp["slider/ygyroscope"] , temp["slider/zgyroscope"])
-    clue_data[2].text = "Magnetic: {} {} {} uTesla".format(temp["slider/xmagnetic"], temp["slider/ymagnetic"], temp["slider/zmagnetic"])
-    clue_data[3].text = "Pressure: {} hPa".format(temp["pressure"])
-    clue_data[4].text = "Altitude: {:.0f} m".format(temp["slider/light"])
-    clue_data[5].text = "Temperature: {} C".format(temp["slider/temperature"])
-    clue_data[6].text = "Humidity: {} %".format(temp["slider/humidity"])
-    clue_data[7].text = "Proximity: {}".format(temp["slider/proximity"])
-    clue_data[8].text = "Color: R: {} G: {} B: {} C: {}".format(temp["slider/red"], temp["slider/green"], temp["slider/blue"])
+    clue_data[0].text = "Accel: {} {} {} m/s^2".format(*(temp["clueSlider/accelXRange"], temp["clueSlider/accelYRange"], temp["clueSlider/accelZRange"]))
+    clue_data[1].text = "Gyro: {} {} {} dps".format(*(temp["clueSlider/gyroXRange"], temp["clueSlider/gyroYRange"], temp["clueSlider/gyroZRange"]))
+    clue_data[2].text = "Magnetic: {} {} {} uTesla".format(*(temp["clueSlider/magneticXRange"], temp["clueSlider/magneticYRange"], temp["clueSlider/magneticZRange"]))
+    clue_data[3].text = "Pressure: {} hPa".format(temp["clueSlider/pressureRange"])
+    clue_data[4].text = "Temperature: {} C".format(temp["clueSlider/tempRange"])
+    clue_data[5].text = "Humidity: {} %".format(temp["clueSlider/humidityRange"])
+    clue_data[6].text = "Proximity: {}".format(temp["clueSlider/proximityRange"])
+    clue_data[7].text = "Color: R:{}G:{}B:{}C:{}".format(*(temp["clueSlider/colorRRange"], temp["clueSlider/colorGRange"], temp["clueSlider/colorBRange"], temp["clueSlider/colorCRange"]))
     clue_data.show()
 
 sensor = {
-    "slider/xaccelerometer" :0
-    "slider/yaccelerometer" : 0
-    "slider/zccelerometer" :0
-    "slider/xgyroscope" :0
-    "slider/ygyroscope" :0
-    "slider/zgyroscope" :0
-    "slider/xmagnetic" :0
-    "slider/ymagnetic" :0
-    "slider/zmagnetic" :0
-    "slider/pressure" :0
-    "slider/temperature" :0
-    "slider/humidity" :0
-    "slider/red" :0
-    "slider/green" :0
-    "slider/blue" :0
-    "slider/proximity" :0
-    "slider/light" :0
+    "clueSlider/accelXRange" : 0,
+    "clueSlider/accelYRange" : 0,
+    "clueSlider/accelZRange" : 0,
+    "clueSlider/gyroXRange" : 0,
+    "clueSlider/gyroYRange" : 0,
+    "clueSlider/gyroZRange" : 0,
+    "clueSlider/magneticXRange" : 0,
+    "clueSlider/magneticYRange" : 0,
+    "clueSlider/magneticZRange" : 0,
+    "clueSlider/pressureRange" : clue.pressure,
+    "clueSlider/tempRange" : clue.temperature,
+    "clueSlider/humidityRange" : clue.humidity,
+    "clueSlider/proximityRange" : clue.proximity,
+    "clueSlider/colorRRange" : 0,
+    "clueSlider/colorGRange" : 0,
+    "clueSlider/colorBRange" : 0,
+    "clueSlider/colorCRange" : 0
 }
 
+# this function holds the condition  if rc is equal to 0 then the client will subscribe then after it will display the text
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        client.subscribe("slider/#")
-        display_text(0)
+        client.subscribe("clueSlider/#")
+        display_text(sensor)
 
+# this function  holds the messages or the payload
 def on_message(client, userdata, msg):
-    display_text(int(msg.payload.decode()))
+    print(msg.topic)
+    print(msg.payload.decode())
+    sensor[msg.topic]= msg.payload.decode()
+    display_text(sensor)
 
-clue.sea_level_pressure = 1020
-
-clue_data = clue.simple_text_display(text_scale=2)
+clue_data = clue.simple_text_display(text_scale=2) 
 
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+client.on_connect = on_connect #calling the function on_connect above
+client.on_message = on_message #calling the function on_message above
 
-client.connect("mqtt.eclipse.org", 1883, 60)
+client.connect("mqtt.eclipseprojects.io", 1883, 60) #connect to the browser
 
-client.loop_forever()
+client.loop_forever() #loop forever
